d5b52d09d095418e9139ec213696c21b6eaed01d
dan9thsense <dan@9thsense.com>
2011-09-24 21:31:41 -0400

    small changes

--- src/main.cpp
+++ src/main.cpp
@@ -105,8 +105,8 @@ White Balance Temperature, Auto
 #define MAX_TILT 1920
 #define DELTA_TILT 320  // corresponds to 5 degrees down, range is +- 30 degrees
 
-#define FIRST_VIDEO_DEVICE "/dev/video0"
-#define SECOND_VIDEO_DEVICE "/dev/video1"
+#define FIRST_VIDEO_DEVICE "/dev/video1"
+#define SECOND_VIDEO_DEVICE "/dev/video0"
 #define SYMBOLIC_VIDEO_DEVICE "/dev/video9"
 #define CAMERA_CHANGE_STRING "cam"
 #define CAMERA_CHANGE_STRING_CAP "Cam"
